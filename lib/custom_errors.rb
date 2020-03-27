48695c1b61a39cdd219459cab2e84c7569eca882
kiran ram <kram@lions.molloy.edu>
2020-03-27 04:28:47 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,13 +9,20 @@ class Person
     self.partner = person
     person.partner = self
   end
+  
+  beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
 
+class PartnerError < StandardError
+end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
 
-
+class PartnerError < StandardError
+end
 
 
